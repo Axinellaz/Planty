9648e3a77e98a3c3440e18d6c3da81c7e882f2b7
Axinella Zehi <axinellazehi@gmail.com>
2023-09-19 12:37:03 +0200

    Avancement page commande. Mise en style du formulaire( 2,3 points a revoir).

--- wp-content/themes/planty/template-page-commande.php
+++ wp-content/themes/planty/template-page-commande.php
@@ -6,64 +6,78 @@ get_header()
 
 
 ?>
-<main> 
+<div class="container-page-commande">
+
+    <main class="commande-main"> 
+        
+        <h2 class="title-commandes"> COMMANDER </h2>
+        <hr class="separateur">
     
-    <h2 class="title-commandes"> Commandes </h2>
-    <hr class="separateur">
-
-    <section class="section-commande">
-        <h3>Votre Commande</h3>
-
-        <div class="direction-row"> 
-
-            <div class="container-vignette">
-            <?php
-
-            include('tastes-tableau.php');
-
-
-                foreach($taste as $key => $valueTab){
-                    echo
-                        $valueTab[ 'img' ].
-                        '<p class="txt-vignette">' .$valueTab[ 'txt' ].'</p>';
-                }
-
-            ?>
+        <section class="section-commande">
+            <h3 class="sous-title-commande">Votre Commande</h3>
+    
+            <div class="direction-row"> 
+    
+                <div class="container-vignette">
+                <?php
+    
+                include('tastes-tableau.php');
+    
+    
+                    foreach($taste as $key => $valueTab){
+                        echo
+                            $valueTab[ 'img' ].
+                            '<p class="txt-vignette">' .$valueTab[ 'txt' ].'</p>';
+                    }
+    
+                ?>
+                </div>
+                
             </div>
+        </section>
+    
+        <hr class="separateur">
+    
+    
+        
+        <form action="" method="post">
             
-        </div>
-    </section>
+            <div class="container-form-flex">
+    
+                <div class="part-form left">
+                    <h3 class="sous-title-commande">Vos informations</h3>
+        
+                    <label class="label-commande" for="nom">Nom</label>
+                    <input class="input-commande" type="text" name="nom" id="nom">
+                
+                    <label class="label-commande" for="prenom">Prénom</label>
+                    <input class="input-commande" type="text" name="prenom" id="prenom">
+                
+                    <label class="label-commande" for="email">E-mail</label>
+                    <input class="input-commande" type="email" name="email" id="email">
+                </div>
+        
+                <div class="separateur-verticale" ></div>
+        
+                <div class="part-form right">
+        
+                    <h3 class="sous-title-commande">Livraison</h3>
+                
+                    <label class="label-commande" for="adresse">Adresse de livraison</label>
+                    <input class="input-commande" type="text" name="adresse" id="adresse">
+                
+                    <label class="label-commande" for="codepostal">Code postal</label>
+                    <input class="input-commande" type="number" name="codepostal" id="codepostal">
+                
+                    <label class="label-commande" for="ville">Ville</label>
+                    <input class="input-commande" type="text" name="ville" id="ville">
+                
+                </div>
+            </div>
+    
+            <input class="btn-envoyer" type="submit" value="Commander">
+        </form>
+    
+    </main>
 
-    <hr>
-
-    <h3>Vos informations</h3>
-
-    <form action="" method="post">
-
-    <input type="text" name="nom" id="nom">
-    <label for="nom">Nom</label>
-
-    <input type="text" name="prenom" id="prenom">
-    <label for="prenom">Prénom</label>
-
-    <input type="email" name="email" id="email">
-    <label for="email">E-mail</label>
-
-    <hr>
-
-    <h3>Livraison</h3>
-
-    <input type="text" name="adresse" id="adresse">
-    <label for="adresse">Adresse de livraison</label>
-
-    <input type="number" name="codepostal" id="codepostal">
-    <label for="codepostal">Code postal</label>
-
-    <input type="text" name="ville" id="ville">
-    <label for="ville">Ville</label>
-
-    <input type="submit" value="Commander">
-    </form>
-
-</main>
-
+</div>    
