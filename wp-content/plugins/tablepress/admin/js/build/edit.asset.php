3cb721db81ff84e406f59feddf4680fada53eb84
Axinella Zehi <axinellazehi@gmail.com>
2023-10-15 00:05:32 +0200

    Nettoyage

--- wp-content/plugins/tablepress/admin/js/build/edit.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '25142384cf03bbf9983c');
--- wp-content/plugins/tablepress/admin/js/build/jspreadsheet.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '71ef3a5a518bc19b1d53');
